3f1df956ef33e5f43ccf6cdefd4d1b61f235118a
sierra182 <sierra-echo@msn.com>
2023-08-26 21:37:52 +0200

    fix get_next_line

--- piscine/pre_stud/get_next_line/get_next_line.c
+++ piscine/pre_stud/get_next_line/get_next_line.c
@@ -3,27 +3,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-int     main(void)
+int     get_next_line(const int fd, char **line)
 {
-    char    *line;
-    char    *file_path;
-    int     fd;
-    int     rslt;
-
-    file_path = "test.txt";
-    if (fd = open(file_path, O_RDONLY) == -1)
-    {
-        write(1, "error file\n", 11);
-        return (1);
-    }
-    while (rslt = get_next_line(fd, &line))
-        printf("%s\n", line);
-    if (rslt == 0)
-        write(1, "end file\n", 9);
-    else 
-    {
-        write(1, "error line\n", 10);
-        return (1);
-    }
-    return (0);
+    
 }
