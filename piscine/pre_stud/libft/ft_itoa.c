1208d5dad4578b57c7080e5005d819a3303dd21c
sierra182 <sierra-echo@msn.com>
2023-09-19 20:07:57 +0200

    add ft_itoa.c

--- piscine/pre_stud/libft/ft_itoa.c
+++ piscine/pre_stud/libft/ft_itoa.c
@@ -1,19 +1,83 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 17:51:10 by seblin            #+#    #+#             */
+/*   Updated: 2023/09/19 20:05:05 by seblin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
+#include "libft.h"
+
+static	char	*ft_alloc_on_rec_end(int is_neg, char *s, int *n_digit)
+{
+	if (!s)
+	{
+		if (is_neg)
+		{
+			s = (char *) malloc(sizeof(char) * (*n_digit + 2));
+			if (!s)
+				return (NULL);
+			s[*n_digit + 1] = '\0';
+			*n_digit = 0;
+			s[*n_digit] = '-';
+		}
+		else
+		{
+			s = (char *) malloc(sizeof(char) * (*n_digit + 1));
+			if (!s)
+				return (NULL);
+			s[*n_digit] = '\0';
+			*n_digit = -1;
+		}
+	}
+	return (s);
+}
 
 char	*ft_itoa(int n)
 {
+	static int	is_neg = 0;	
+	static int	n_digit = 0;
+	char		*s;
+	int			div;
+	int			mod;
 
+	s = NULL;
+	n_digit++;
+	if (n == -2147483648)
+		return (s = "-2147483648");
+	if (n < 0)
+	{
+		is_neg = 1;
+		n = -n;
+	}
+	div = n / 10;
+	mod = n % 10;
+	if (n > 9)
+		s = ft_itoa(div);
+	s = ft_alloc_on_rec_end(is_neg, s, &n_digit);
+	if (!s)
+		return (NULL);
+	n_digit++;
+	s[n_digit] = (char) mod + 48;
+	return (s);
 }
-
+/*
+#include <stdio.h>
 int	main(void)
-{
+{  
 	char	*s;
 	int	pos = 422489;
-	int neg = -9978425;
+	int neg = -2147483648;
 
-	s = ft_itoa(pos);
-	printf("%s", s);
-	//free(s);
+	s = NULL;
+	s = ft_itoa(neg); 
+	printf("%s\n", s);
+	free(s);	
 	return (0);
 }
+*/
--- piscine/pre_stud/libft/libft.h
+++ piscine/pre_stud/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 10:42:53 by seblin            #+#    #+#             */
-/*   Updated: 2023/09/19 15:44:52 by seblin           ###   ########.fr       */
+/*   Updated: 2023/09/19 19:56:43 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
