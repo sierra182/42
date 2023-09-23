e654ded1d5a798a3ae41c64ca8d0088da2cef3d0
sierra182 <sierra-echo@msn.com>
2023-09-23 13:34:16 +0200

    WIP libft bonus

--- piscine/pre_stud/libft/ft_lstadd_front_bonus.c
+++ piscine/pre_stud/libft/ft_lstadd_front_bonus.c
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/22 21:10:03 by seblin            #+#    #+#             */
-/*   Updated: 2023/09/23 13:02:48 by seblin           ###   ########.fr       */
+/*   Created: 2023/09/23 13:10:28 by seblin            #+#    #+#             */
+/*   Updated: 2023/09/23 13:13:57 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	t_list	*new_l;
-
-	new_l = (t_list *) malloc (sizeof(t_list));
-	new_l->content = content;
-	new_l->next = NULL;
-	return (new_l);
+	*lst->next = new;
 }
-/*
-int	main(void)
-{
-	void	*content;
-	t_list	*list = ft_lstnew(content);
-	return (0);
-}
-*/
--- piscine/pre_stud/libft/libft.h
+++ piscine/pre_stud/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 10:42:53 by seblin            #+#    #+#             */
-/*   Updated: 2023/09/23 10:01:54 by seblin           ###   ########.fr       */
+/*   Updated: 2023/09/23 12:48:07 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
+
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
@@ -55,5 +56,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+t_list	*ft_lstnew(void *content);
 
 #endif
